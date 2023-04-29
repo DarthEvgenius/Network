239c51b3b4ba390504decd52870e6bc2a823bc3b
DarthEvgenius <clay14@yandex.ru>
2023-04-29 22:05:51 +0300

    Edit post fix bug of several edits

--- project4/network/static/network/index.js
+++ project4/network/static/network/index.js
@@ -61,7 +61,6 @@ async function submit_post(post_id) {
 	// .then(response => response.text())
 	// .then(data => {
 	// 	const content = data
-	// 	console.log(content);
 	// 	return content;
 	// })
 
@@ -74,37 +73,28 @@ async function submit_post(post_id) {
 }
 
 
-// Make a post with new content
-function new_post(post_id, edit_btn) {
-
-	submit_post(post_id).then(content => {
-		
-		// Fill in the new content
-		const id_content = 'content_' + post_id;
-		element = document.querySelector(`#${id_content}`);
-
-		element.innerHTML = '';
-		element.innerHTML = content;
-	});
+// Make a post with new content, remove form
+function new_post(post_id) {
 
 	// Hide the form
 	const form_id = 'form_' + post_id;
 	const form = document.querySelector(`#${form_id}`);
 	form.display = 'none';
 
-	// // Fill in the new content
-	// const id_content = 'content_' + post_id;
-	// element = document.querySelector(`#${id_content}`);
-
-	// element.innerHTML = '';
-	// element.innerHTML = new_content;
-
-	// Show edit button
-	edit_btn.style.display = 'inline-block';
+	// Send new content to the server
+	submit_post(post_id).then(content => {		
+		// Fullfill the content element
+		// Get id of content element
+		const id_content = 'content_' + post_id;
+		content_element = document.querySelector(`#${id_content}`);
+		// Fill in the new content
+		content_element.innerHTML = '';
+		content_element.innerHTML = content;
+	});
 }
 
 // Add new form to the element, filled with content
-function add_form(element_content, old_content, edit_btn) {
+function add_form(content_element, old_content, post_id) {
 	// Create input form
 	const form = document.createElement('form');
 	form.id = 'form_' + post_id;
@@ -121,22 +111,20 @@ function add_form(element_content, old_content, edit_btn) {
 	const submit = document.createElement('button');
 	submit.className = 'btn btn-secondary';
 	submit.id = 'submit_' + post_id;
-	submit.innerHTML = 'Save';
-
-	// Prevent page from reloading and send data
-	submit.addEventListener('click', function(event) {
-		event.preventDefault();
-		// const content = submit_post(post_id);
-		
-		new_post(post_id, edit_btn);
-	});
+	submit.innerHTML = 'Save';	
 
 	// Add new form to the page
 	form_group.appendChild(textarea);
 	form.appendChild(form_group);
 	form.appendChild(submit);
-	element_content.innerHTML = ``;
-	element_content.appendChild(form);
+	content_element.innerHTML = ``;
+	content_element.appendChild(form);
+
+	// Prevent page from reloading and send data
+	submit.addEventListener('click', function(event) {
+		event.preventDefault();
+		new_post(post_id);
+	});
 }
 
 // Make a text-area out of post and change content of post
@@ -147,17 +135,16 @@ function edit(edit_btn, post_id) {
 
 	// Get the element with post content
 	const id_content = 'content_' + post_id;
-	element_content = document.querySelector(`#${id_content}`);
+	content_element = document.querySelector(`#${id_content}`);
 
 	// Get old content of the post
-	const old_content = element_content.innerHTML;
+	const old_content = content_element.innerHTML;
 
-	// Create textarea out with the old content of the post
-	add_form(element_content, old_content, edit_btn);
-	
-	// Send new content to the server via button
-	
-	
+	// Create textarea with the old content of the post
+	add_form(content_element, old_content, post_id);
+
+	// Show edit button
+	edit_btn.style.display = 'inline-block';
 }
 
 
@@ -175,15 +162,32 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// Edit post listener
-	document.addEventListener('click', event => {
-		// Get the element that triggered an event
-		const edit_btn = event.target;
 
-		if (edit_btn.className === 'edit') {
-			// Get the post id via data attr
-			post_id = edit_btn.dataset.id;
-			edit(edit_btn, post_id);
-		}
-	})
-	
+	// 1st variant doesn't work with several edit events
+	// Because id variable got rewritten
+
+	// document.addEventListener('click', event => {
+	// 	// Get the element that triggered an event
+	// 	const edit_btn = event.target;
+
+	// 	if (edit_btn.className === 'edit') {
+	// 		// Get the post id via data attr
+	// 		post_id = edit_btn.dataset.id;
+	// 		// Edit post
+	// 		edit(edit_btn, post_id);
+	// 	}
+	// });
+
+
+	// Add listener to each button explicitly!
+	// Get list of buttons
+	const edit_buttons = document.querySelectorAll('.edit');
+
+	edit_buttons.forEach((btn) => {
+		// Set listener
+		btn.addEventListener('click', () => {
+			edit(btn, btn.dataset.id);
+		});
+	});
+
 })
